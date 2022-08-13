0ffc15ce2a905dffd1b5099f862323d3829f082f
Your Name <you@example.com>
2022-08-13 23:50:02 +0300

    Create CategoryScreen

--- screens/CategoryScreen.mjs
+++ screens/CategoryScreen.mjs
@@ -1,4 +1,4 @@
-import {Scenes} from "telegraf";
+import {Markup, Scenes} from "telegraf";
 import CategoryService from "../core/service/CategoryService.mjs";
 
 const CategoryScreen = new Scenes.BaseScene('CategoryScreen');
@@ -6,10 +6,25 @@ const categoryService = new CategoryService();
 
 CategoryScreen.enter(async (ctx) => {
     const categories = await categoryService.getCategories();
+    let message = ``;
     const buttons = [];
     let tempButtons = [];
 
-    categories.map()
+    categories.map((item, index) => {
+        message += `⚪ <b>${item.name}:</b> ${item.description}\n\n`;
+
+        if (index % 2) {
+            tempButtons.push(Markup.button.callback('⚪ ' + item.name, `${index.id}`))
+            buttons.push(tempButtons);
+            tempButtons = [];
+        } else {
+            tempButtons.push(Markup.button.callback('⚪ ' + item.name, `${index.id}`))
+        }
+    });
+
+    await ctx.replyWithHTML(message, Markup.inlineKeyboard(buttons));
 });
+
+// 🟢
 
 export default CategoryScreen;
